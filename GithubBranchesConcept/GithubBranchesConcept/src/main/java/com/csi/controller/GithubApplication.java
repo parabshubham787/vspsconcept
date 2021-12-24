23614dc0ef947c8c1baa4a2a3616373c6ce59f39
parabshubham787 <parabshubham787@gmail.com>
2021-12-24 10:58:51 +0530

    Initial Commit

---
+++ GithubBranchesConcept/GithubBranchesConcept/src/main/java/com/csi/controller/GithubApplication.java
@@ -0,0 +1,17 @@
+package com.csi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class GithubApplication {
+
+    @GetMapping()
+    public ResponseEntity<String> heyHello()
+    {
+        return ResponseEntity.ok("VSPS Concept");
+    }
+}
